7c88d0100aa8483ddb3a4ef61f74c4307547a395
Tyriq <C00439145@louisiana.edu>
2022-05-24 10:35:42 -0500

    Added Task and Toggle components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,48 @@
+import {useState} from 'react';
 import Header from "./components/Header";
 import Tasks from "./components/Tasks"
+
+
 function App() {
+  
+  // eslint-disable-next-line
+  const [tasks,setTasks] = useState([
+    {
+        id: 1,
+        text: 'Doctor Appointment',
+        day: 'Feb 5th at 2:30pm',
+        reminder: true
+    },
+    {
+        id: 2,
+        text: 'Meeting at School',
+        day: 'Feb 6th at 1:30pm',
+        reminder: true
+    },
+    {
+        id: 3,
+        text: 'Food Shopping at 2:30',
+        day: 'Feb 5th at 2:30pm',
+        reminder: false
+    }])
+
+    //Delete
+    const deleteTask = (id) => {
+      console.log('delete',id);
+      setTasks(tasks.filter((task => task.id !== id)));
+    }
+
+    const toggleReminder = (id) => {
+      setTasks(tasks.map((task) => task.id === id ? {...task, reminder: !task.reminder}: task));
+      console.log("toggle",id);
+    }
+
+
 
   return (
     <div className="container">
      <Header />
-     <Tasks />
+     <Tasks tasks = {tasks} onDelete = {deleteTask} onToggle = {toggleReminder}/>
     </div>
   );
 }
